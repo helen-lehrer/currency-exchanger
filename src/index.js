4d0f6f5c6b16ade57e46642f89408c088dec3f7d
Helen Lehrer <hlehrer@outlook.com>
2022-09-09 11:22:30 -0700

    add HTML & index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import { Currency } from './../src/js/currency-service.js';
-
 import { CurrencyExchange } from './../src/js/currency-service.js';
+
+function exchange() {
+  CurrencyExchange.exchange()
+    .then(function(response) {
+      if (response.conversion_rate) {
+        printElements(response);
+      } else {
+        printError(response);
+      }
+    });
+}
+
+function printElements(response) {
+  
+
+};
+
+function printError(error) {
+
+};
