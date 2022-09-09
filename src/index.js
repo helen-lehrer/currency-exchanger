0b169e4b231b87ae97384ae75700eccfb3ec0e57
Helen Lehrer <hlehrer@outlook.com>
2022-09-09 12:29:10 -0700

    lunch break & add dotenv-webpack

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { CurrencyExchange } from './../src/js/currency-service.js';
 function exchange() {
   CurrencyExchange.exchange()
     .then(function(response) {
-      if (response.conversion_rate) {
+      if (response.conversion_rates) {
         printElements(response);
       } else {
         printError(response);
@@ -13,10 +13,19 @@ function exchange() {
 }
 
 function printElements(response) {
-  
+  document.querySelector('#answer').innerText = `${response.conversion_rates}`;
 
-};
+}
 
 function printError(error) {
+  document.querySelector('#answer').innerText = `There was an error accessing the conversion data: 
+  ${error}.`;
+}
 
-};
+const handleFormSubmission = () => {
+  exchange();
+  let currency = new Currency();
+  currency.dollar = 5;
+}
+
+document.querySelector('#calculate').addEventListener("click", handleFormSubmission);
